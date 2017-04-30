cdbd680e357c66daebc1fde76c75dc9b1b038f00
dameinzh <dameinzh@gmail.com>
2017-05-01 01:39:02 +0200

    commit

--- test_src/im_test.py
+++ test_src/im_test.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import argrelmax
+
 
 MAX_DISTANCE = 4000
 MIN_DISTANCE = 350
@@ -19,6 +19,7 @@ def depth_process(img):
     img_c = depth_filter(img, 5)
     bins = np.linspace(MIN_DISTANCE, MAX_DISTANCE, 100)
     hist = cv2.calcHist([img_c], [0], None, [100], [MIN_DISTANCE, MAX_DISTANCE])
+    segs = histo_segment(hist, 100)
     hist_g = np.gradient(hist, axis=0)
     print(np.median(hist))
     # col_median = np.median(im, axis=0)
@@ -35,23 +36,21 @@ def depth_process(img):
     plt.show()
 
 
+def depth_thresh(img, low_thresh, high_thresh, convert=False):
+    img[img < low_thresh] = 0
+    img[img > high_thresh] = 0
+    if convert:
+        img = img.astype(np.uint8)
+
+    return img
+
+
 def histo_segment(hist, thresh_low):
 
     hist = np.reshape(hist, -1)
     # find all the indices above low threshold
     inds = np.nonzero(hist > thresh_low)[0]
-
-    # find non-continuous indices and add start end boundary
-    diffs = np.diff(inds)
-    hist_inds = np.nonzero(diffs > 1)[0]
-    hist_inds = np.hstack((np.hstack((0, hist_inds)), inds.size-1))
-
-    # find the segments of continuous indices
-    segments = np.array([0, 0])
-    for j in range(hist_inds.size-1):
-        if hist_inds[j+1] - hist_inds[j] > 1:
-            segments = np.vstack((segments, (hist_inds[j], hist_inds[j+1])))
-    segments = segments[1:, ]
+    segments = np.split(inds, np.where(np.diff(inds) != 1)[0] + 1)
 
     return segments
 
@@ -67,17 +66,17 @@ def depth_filter(img, option):
                     5: closing
     :return: 
     """
-    if option == 0:
+    if option == 0 or option == 'median':
         img_p = cv2.medianBlur(img, ksize=5)
-    elif option == 1:
+    elif option == 1 or option == 'gaussian':
         img_p = cv2.GaussianBlur(img, (5, 5), 0)
-    elif option == 2:
+    elif option == 2 or option == 'dilation':
         img_p = cv2.dilate(img, kernel=np.ones((5, 5), np.uint16), iterations=1)
-    elif option == 3:
+    elif option == 3 or option == 'erosion':
         img_p = cv2.erode(img, kernel=np.ones((5, 5), np.uint16), iterations=1)
-    elif option == 4:
+    elif option == 4 or option == 'opening':
         img_p = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel=np.ones((5, 5), np.uint16))
-    elif option == 5:
+    elif option == 5 or option == 'closing':
         img_p = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel=np.ones((5, 5), np.uint16))
 
     return img_p
@@ -99,13 +98,28 @@ def scanline(img, subWindow):
     return s
 
 
+def remove_ground(d_img, height):
+    V_FOV = 46  # degree
+    H_FOV = 59  # degree
+    v_num, h_num = d_img.shape
+    v_rad = np.radians(np.linspace(V_FOV/2, -V_FOV/2, v_num))
+    h_rad = np.radians(np.linspace(-H_FOV/2, H_FOV/2, h_num))
+    b = height / np.tan(v_rad)
+    a = b * np.tan(h_rad)
+    aa, bb = np.meshgrid(a, b)
+    cc = np.ones(aa.shape) * height
+    dd = np.sqrt(aa**2 + bb**2 + cc**2)
+
+    return dd
+
+
 if __name__ == '__main__':
-    imd_filename = 'test_data/depth_image_dc4_20170408-170421.png' # box
-    # imd_filename = 'test_data/depth_image_dc4_20170408-171930.png' # doorway
-    # imd_filename = 'test_data/depth_image_dc4_20170408-171342.png' # table
+    imd_filename = 'test_data/depth_image_dc4_20170408-170421.png'  # box
+    imd_filename = 'test_data/depth_image_dc4_20170408-171930.png'  # doorway
+    imd_filename = 'test_data/depth_image_dc4_20170408-171342.png'  # table
     imc_filename = 'test_data/color_image20170408-170543.png'
     im_d = cv2.imread(imd_filename, -1)
     im_c = cv2.imread(imc_filename, 3)
 
     depth_process(im_d)
-    # color_process(im_c)
+    color_process(im_c)
--- test_src/test1.py
+++ test_src/test1.py
@@ -13,8 +13,8 @@ rospy.init_node('test_node')
 
 
 dp_im = rospy.wait_for_message('/camera/depth/image_raw', Image)
-c_im = rospy.wait_for_message('/camera/color/image_raw', Image)
-clouds = rospy.wait_for_message('/camera/depth/points', PointCloud2)
+c_im = rospy.wait_for_message('/camera/rgb/image_raw', Image)
+# clouds = rospy.wait_for_message('/camera/depth/points', PointCloud2)
 
 b = cv_bridge.CvBridge()
 
@@ -22,7 +22,7 @@ im_d = b.imgmsg_to_cv2(dp_im, desired_encoding='passthrough')
 im_c = b.imgmsg_to_cv2(c_im, desired_encoding='bgr8')
 
 print type(im_d)
-print type(im_c)
+# print type(im_c)
 
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
@@ -35,13 +35,13 @@ cv2.imwrite(d_filename, im_d)
 #cv2.waitKey(0)
 #np.savetxt('sample_depth_image', im)
 
-p = np.empty((640*480, 3))
-i = 0
-for pts in point_cloud2.read_points(clouds, skip_nans=True):
-    p[i, 0] = pts[0]
-    p[i, 1] = pts[1]
-    p[i, 2] = pts[2]
-    i += 1
-
-print(p.dtype)
-np.savetxt(pc_filename, p)
+# p = np.empty((640*480, 3))
+# i = 0
+# for pts in point_cloud2.read_points(clouds, skip_nans=True):
+#     p[i, 0] = pts[0]
+#     p[i, 1] = pts[1]
+#     p[i, 2] = pts[2]
+#     i += 1
+#
+# print(p.dtype)
+# np.savetxt(pc_filename, p)
